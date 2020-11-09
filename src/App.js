81180bd9dd57aa6931bdb4bf8e4ca68363be08f5
belferink1996 <ben.elferink@icloud.com>
2020-11-09 20:52:42 +0200

    create and publish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import logo from './media/logo.svg';
+import './style/style.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
+
+  useEffect(() => {
+    let str = '';
+
+    for (let i = input.length - 1; i >= 0; i--) {
+      str += input.charAt(i);
+    }
+
+    setOutput(str);
+  }, [input]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <img src={logo} alt='logo' />
+      <input
+        className='input'
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+      <input className='output' value={output} readOnly />
+      <footer>
+        Developed for{' '}
+        <a href='http://orandt.co.il' target='blank'>
+          OR&T
+        </a>{' '}
+        &copy; 2020
+      </footer>
     </div>
   );
 }
