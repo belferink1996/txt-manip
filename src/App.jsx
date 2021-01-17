3ae9d2cbd6aa0dfa59b48ebad3dc78d808d4cf97
Ben Elferink <ben.elferink@icloud.com>
2021-01-18 00:23:54 +0200

    pulled App from components folder

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './style/style.css';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div className='app'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/style.css';
-import App from './components/App/App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
